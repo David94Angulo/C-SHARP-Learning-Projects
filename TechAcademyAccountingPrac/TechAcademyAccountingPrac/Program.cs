7d1d5977ec82c5d3d32ad4b5e5d9a221b0216435
David Angulo <david.angulo@sbcglobal.net>
2019-04-26 15:18:59 -0700

    Add Accounting Practice

---
+++ TechAcademyAccountingPrac/TechAcademyAccountingPrac/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+
+namespace TechAcademyAccountingPrac
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to Acme Accounting Systems");
+            Console.WriteLine("Remember, we're \"accounting\" on you!");
+            Console.WriteLine("(Press Enter to Close this Window)");
+            Console.Read();
+
+        }
+    }
+}
