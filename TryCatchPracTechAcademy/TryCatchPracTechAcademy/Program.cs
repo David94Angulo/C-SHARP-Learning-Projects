b04a7a59d4a8a4ffb68d209483366488ee60ebe6
David Angulo <david.angulo@sbcglobal.net>
2019-05-04 00:48:47 -0700

    Add TryCatch Project

---
+++ TryCatchPracTechAcademy/TryCatchPracTechAcademy/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryCatchPracTechAcademy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                List<int> Numbers = new List<int> {410,345,6,3,13,51,3,};
+                Console.WriteLine("Please enter an Integer");
+                int Divider = Convert.ToInt32(Console.ReadLine());
+                foreach (int Number in Numbers)
+                {
+                    Console.WriteLine(Number / Divider);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+
+            Console.WriteLine("Welcome to the other side of the Try block!");
+
+            Console.ReadLine();
+
+        }
+    }
+}
