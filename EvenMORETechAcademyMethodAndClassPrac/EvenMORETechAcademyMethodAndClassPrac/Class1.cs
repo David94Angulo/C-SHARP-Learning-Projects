7e5e2267deb1297582e801d327fe7bfc3bec29a7
David Angulo <david.angulo@sbcglobal.net>
2019-05-06 01:37:06 -0700

    Add evenMOREpractice

---
+++ EvenMORETechAcademyMethodAndClassPrac/EvenMORETechAcademyMethodAndClassPrac/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvenMORETechAcademyMethodAndClassPrac
+{
+    public class Class1
+    {
+
+        public static int OneOptional(int Xone, int Xtwo = 10)
+        {
+
+            int Sum = Xone * Xtwo;
+            return Sum;
+        }
+
+
+
+
+    }
+}
---
+++ EvenMORETechAcademyMethodAndClassPrac/EvenMORETechAcademyMethodAndClassPrac/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvenMORETechAcademyMethodAndClassPrac
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+
+            
+           Class1 FirstOptionalNumber = new Class1();
+
+
+
+           Console.WriteLine("Please type in a number: ");
+           int n = Convert.ToInt16(Console.ReadLine());
+
+           Console.WriteLine("Please type in another number IF YOU DARE >;D : ");
+            string input = Console.ReadLine();
+
+            if (input == "")
+            {
+                Console.WriteLine("Here is the result of your numbers: " + Class1.OneOptional(n));
+            }
+
+            else
+            {
+                int m = Convert.ToInt16(input);
+
+                Console.WriteLine("Here is the result of your numbers: " + Class1.OneOptional(n, m));
+            }
+            
+
+          
+           Console.ReadLine();
+            
+        }
+    }
+}
