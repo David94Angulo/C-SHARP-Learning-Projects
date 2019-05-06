4c97cb3716db516e557bba7b711269ff110818e3
David Angulo <david.angulo@sbcglobal.net>
2019-05-06 01:06:28 -0700

    add 3 method call practice

---
+++ MOREClassAndMethodPracTech/MOREClassAndMethodPracTech/Class1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MOREClassAndMethodPracTech
+{
+    public class Class1
+    {
+
+
+
+        public static int Number(int a)
+        {
+            a = (a * 2) + 6;
+            return a;
+        }
+
+        public static int Number(double a)
+        {
+            a = (a - 12) / 5;
+            int A = Convert.ToInt16(a);
+            return A;
+        }
+
+
+
+
+        public static int Number (String a)
+        {
+            int A = Convert.ToInt16(a);
+           A = (A + 5) + (5 + 5) / (5 * 4);
+            return A;
+        }
+
+
+
+
+
+    }
+}
---
+++ MOREClassAndMethodPracTech/MOREClassAndMethodPracTech/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MOREClassAndMethodPracTech
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Class1 Step1 = new Class1();
+            Console.WriteLine("The result is: " + Class1.Number(a: 12));
+
+
+
+            Class1 Step2 = new Class1();// <--- Is this "Instantiation really needed? Maybe If I got a Tech Academy definition of instationation, It would help alot.
+            Console.WriteLine("The result is: " + Class1.Number(a: 12.5));
+
+
+            Class1 Step3 = new Class1();
+            Console.WriteLine("The result is: " + Class1.Number(a: "125"));
+
+
+            Console.ReadLine();
+        }
+    }
+}
