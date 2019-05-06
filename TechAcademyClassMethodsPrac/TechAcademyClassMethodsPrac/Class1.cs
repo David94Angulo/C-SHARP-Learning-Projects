272da5a9ffd8220ddb09d11f8c21d508ae94c7cb
David Angulo <david.angulo@sbcglobal.net>
2019-05-06 02:20:17 -0700

    Add ClassMethodsPractice

---
+++ TechAcademyClassMethodsPrac/TechAcademyClassMethodsPrac/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TechAcademyClassMethodsPrac
+{
+    class Class1
+    {
+
+
+
+        public void PrintInt (int x)
+        {
+            Console.WriteLine("Your number divided by 2 is: "+ x/2);
+        }
+
+        public void Outputs (out int G, out string Question)
+        {
+            G = 7;
+            Question = "How?";
+        }
+
+        public static void Outputs(out double g)
+        {
+            g = 21.1;
+        }
+
+    }
+}
---
+++ TechAcademyClassMethodsPrac/TechAcademyClassMethodsPrac/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TechAcademyClassMethodsPrac
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Class1 Trial1 = new Class1();
+            Console.WriteLine("Please enter a number: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Trial1.PrintInt(n);
+
+
+
+
+            Console.ReadLine();
+        }
+
+
+    }
+}
