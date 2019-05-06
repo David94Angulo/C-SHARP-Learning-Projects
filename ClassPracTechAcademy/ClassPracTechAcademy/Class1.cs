67e0db7dc54767c0bba85af88a0beed312270649
David Angulo <david.angulo@sbcglobal.net>
2019-05-06 00:46:05 -0700

    Add ClassPractice

---
+++ ClassPracTechAcademy/ClassPracTechAcademy/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassPracTechAcademy
+{
+    public class MathStuff
+    {
+
+
+        public static void SomeMath(int Num1, int Num2)
+        {
+            Num1 = (Num1 + (5 - 6) * 8 / (24 * 45));
+            Console.WriteLine(Num2);
+        }
+
+
+
+    }
+}
---
+++ ClassPracTechAcademy/ClassPracTechAcademy/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassPracTechAcademy
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            MathStuff FirstSet = new MathStuff();
+
+            MathStuff.SomeMath( 5, 133);
+
+            MathStuff.SomeMath(Num1: 50, Num2: 100);
+
+        }
+    }
+}
