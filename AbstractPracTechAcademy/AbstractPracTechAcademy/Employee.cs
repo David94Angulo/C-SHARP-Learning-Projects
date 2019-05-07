f8274fdb23c9a1585520a7cb0770e05dd24200b4
David Angulo <david.angulo@sbcglobal.net>
2019-05-07 15:19:46 -0700

    Add Abstract practice

---
+++ AbstractPracTechAcademy/AbstractPracTechAcademy/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractPracTechAcademy
+{
+    public class Employee: Person
+    {
+
+        public void SayName()
+        {
+            string FullName = FirstName + " " + LastName;
+            Console.WriteLine("Hello, my  name is " + FullName);
+        }
+
+
+    }
+}
---
+++ AbstractPracTechAcademy/AbstractPracTechAcademy/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractPracTechAcademy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Employee Charles = new Employee();
+            Charles.FirstName = "Sample";
+            Charles.LastName = "Student";
+
+            Charles.SayName();
+
+
+            Console.ReadLine();
+        }
+    }
+}
