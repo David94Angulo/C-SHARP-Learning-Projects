be20a5c957c08cfc2f3d5bb3ee3cbda9e9bdc28a
David Angulo <david.angulo@sbcglobal.net>
2019-05-07 13:04:38 -0700

    Add People Project

---
+++ People/People/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace People
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Employee Manager = new Employee();
+
+            Manager.FirstName = "Sample";
+            Manager.LastName = "Student";
+
+            Manager.SayName();
+
+            Console.ReadLine();
+
+        }
+    }
+}
