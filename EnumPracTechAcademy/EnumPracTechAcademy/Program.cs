2dbbc5bc3204425e15091720c7a51cc944bd299a
David Angulo <david.angulo@sbcglobal.net>
2019-05-09 23:39:21 -0700

    Add Enum Practice

---
+++ EnumPracTechAcademy/EnumPracTechAcademy/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumPracTechAcademy
+{
+    class Program
+    {
+
+        public enum DayOfWeek
+        {
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
+        }
+
+        static void Main(string[] args)
+        {
+           
+            try
+            {
+                Console.WriteLine("Please enter the current Day of the week: ");
+                string Input = Console.ReadLine();
+
+                DayOfWeek CurrentDay = (DayOfWeek)Enum.Parse(typeof(DayOfWeek), Input);
+
+                Console.WriteLine("Alright, Today is " + CurrentDay);
+            }
+
+            catch 
+            { 
+
+                Console.WriteLine("Please enter an actual day of the week");
+            }
+          
+        
+            Console.ReadLine();
+        }
+
+
+       
+
+
+    }
+}
