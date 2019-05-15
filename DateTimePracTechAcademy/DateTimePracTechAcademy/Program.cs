6b0fbb79dfd442757fe4d28d373ee88868455d15
David Angulo <david.angulo@sbcglobal.net>
2019-05-15 03:35:19 -0700

    add datetimepractice

---
+++ DateTimePracTechAcademy/DateTimePracTechAcademy/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace DateTimePracTechAcademy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DateTime.Now);
+
+            Console.WriteLine("please enter a number: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            TimeSpan timetoadd = TimeSpan.FromHours(number);
+            TimeSpan date = DateTime.Now.TimeOfDay;
+            var hoursfromnow = date.Add(timetoadd);
+
+
+            Console.WriteLine("The time in {0} hours will be {1}",number,hoursfromnow);
+
+            Console.ReadLine();
+        }
+    }
+}
