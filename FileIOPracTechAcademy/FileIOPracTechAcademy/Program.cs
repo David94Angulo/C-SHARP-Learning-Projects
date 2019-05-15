8ea1c07b61390f39e2de5378af9a90b3fad41103
David Angulo <david.angulo@sbcglobal.net>
2019-05-15 02:47:11 -0700

    add practice file io

---
+++ FileIOPracTechAcademy/FileIOPracTechAcademy/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FileIOPracTechAcademy
+{
+     class Program
+    {
+         static void Main()
+        {
+            Console.WriteLine("Type in a number: ");
+             string number = Console.ReadLine();
+            File.WriteAllText(@"FileIO.txt", number);
+            //File.ReadAllText(@"C:\Users\david\C-SHARP-Learning-Projects\FileIOlogs.txt");
+            using (StreamReader readtext = new StreamReader(@"FileIO.txt"))
+            {
+                string line = readtext.ReadLine();
+                Console.WriteLine(line);
+
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
