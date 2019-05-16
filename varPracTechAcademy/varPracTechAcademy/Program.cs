3db29745877b34f6d8a30f9d920384f563227dc1
David Angulo <david.angulo@sbcglobal.net>
2019-05-16 02:49:09 -0700

    addpractice

---
+++ varPracTechAcademy/varPracTechAcademy/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace varPracTechAcademy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string Name = "Mr.Sexy";
+
+            var RealName = "Charles";
+
+
+        }
+    }
+
+    public class sampleClass
+    {
+        public sampleClass() : this(71)
+        {
+        }
+
+        public sampleClass(int Num)
+        {
+        }
+
+    }
+}
