06829b7ada26b69e77eef17685372fb0e3be2a98
David Angulo <david.angulo@sbcglobal.net>
2019-05-16 04:05:33 -0700

    Add exception Handling

---
+++ UserExceptionPracTechAcademy/UserExceptionPracTechAcademy/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserExceptionPracTechAcademy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+
+                int Age = 0 ;
+                bool isValid = false;
+                while (!isValid)
+                {
+                    Console.WriteLine("Please enter your age: ");
+                    isValid = int.TryParse(Console.ReadLine(), out Age);
+                    Console.WriteLine("Enter digits. No decimals, no letters please.\n\n");
+                   
+                }
+
+                if ( Age < 0)
+                {
+                    throw new TrollException();
+                }
+
+                DateTime StartDate = DateTime.Now;
+                DateTime yearBorn = StartDate.AddYears(-(Age));
+
+                Console.WriteLine("You were born in {0}!", yearBorn.Year);
+            }
+
+            catch(TrollException)
+            {
+                Console.WriteLine("Ha. Ha. Very funny, you can't be Negative years old...");
+            }
+
+
+            catch(Exception)
+            {
+                Console.WriteLine("An unexpected error occured, please try running the program again.");
+                Console.ReadLine();
+                return;
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ UserExceptionPracTechAcademy/UserExceptionPracTechAcademy/TrollException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserExceptionPracTechAcademy
+{
+     public class TrollException : Exception
+     {
+         public TrollException()
+             : base() { }
+         public TrollException(string message)
+             : base(message) { }
+     }
+}
